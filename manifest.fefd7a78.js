a266b0516c71d4cfe168c58a25db6862ec2271cf
Erick Katzenstein <erick.katzenstein@gmail.com>
2017-07-28 10:59:50 -0400

    Updates

--- manifest.fefd7a78.js
+++ manifest.fefd7a78.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"demo"}[chunkId]||chunkId) + "." + {"0":"3917b49a"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"demo"}[chunkId]||chunkId) + "." + {"0":"cfdb617f"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
@@ -148,4 +148,4 @@
 /******/ })
 /************************************************************************/
 /******/ ([]);
-//# sourceMappingURL=manifest.45f474a0.js.map
+//# sourceMappingURL=manifest.fefd7a78.js.map
